a16b504b68a36b0fd75d22331125424ce9a71545
Lua <47286632+Luatiq@users.noreply.github.com>
2022-11-01 16:07:13 +0100

    added modal to confirm deletion

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,33 @@
 import './styles/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'tingle.js/dist/tingle.min.css';
 
 // JS
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '@fortawesome/fontawesome-free/js/all.min';
+
+const Tingle = require('tingle.js/dist/tingle.min');
+
+window.confirmDelete = function(el) {
+    let elData = el.dataset;
+    const modal = new Tingle.modal({
+        footer: true,
+        closeMethods: ['overlay', 'escape'],
+        cssClass: ['confirmModal'],
+        onClose() {modal.destroy()},
+    });
+
+    modal.setContent(`<h1>${elData.delConfirmText}</h1>`);
+    modal.addFooterBtn(elData.delCancelText, 'btn btn-secondary mx-1', () => {modal.close()});
+    modal.addFooterBtn(elData.delButtonText, 'btn btn-danger', function () {
+        window.location = elData.delPath.replace('0', elData.delId);
+    });
+
+    modal.open();
+}
+
+let confirmDeleteElements = document.getElementsByClassName('confirmDelete');
+for (let i = 0; i < confirmDeleteElements.length; i++) {
+    confirmDeleteElements[i].setAttribute('onclick', 'confirmDelete(this)')
+}
