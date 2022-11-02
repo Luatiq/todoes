2ed045dbb2b6a4846bd71a7ddf8da2d9d06a5a33
Lua <47286632+Luatiq@users.noreply.github.com>
2022-11-02 10:33:43 +0100

    delete modal; is now more generic "confirmModal"

--- assets/app.js
+++ assets/app.js
@@ -10,7 +10,7 @@ import '@fortawesome/fontawesome-free/js/all.min';
 
 const Tingle = require('tingle.js/dist/tingle.min');
 
-window.confirmDelete = function(el) {
+window.confirmModal = function(el) {
     let elData = el.dataset;
     const modal = new Tingle.modal({
         footer: true,
@@ -19,16 +19,11 @@ window.confirmDelete = function(el) {
         onClose() {modal.destroy()},
     });
 
-    modal.setContent(`<h1>${elData.delConfirmText}</h1>`);
-    modal.addFooterBtn(elData.delCancelText, 'btn btn-secondary mx-1', () => {modal.close()});
-    modal.addFooterBtn(elData.delButtonText, 'btn btn-danger', function () {
-        window.location = elData.delPath.replace('0', elData.delId);
+    modal.setContent(`<h1>${elData.confText}</h1>`);
+    modal.addFooterBtn(elData.confCancelText, 'btn btn-secondary mx-1', () => {modal.close()});
+    modal.addFooterBtn(elData.confButtonText, `btn ${elData.confButtonClass}`, function () {
+        window.location = elData.confPath;
     });
 
     modal.open();
-}
-
-let confirmDeleteElements = document.getElementsByClassName('confirmDelete');
-for (let i = 0; i < confirmDeleteElements.length; i++) {
-    confirmDeleteElements[i].setAttribute('onclick', 'confirmDelete(this)')
 }
